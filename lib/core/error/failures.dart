7a33d1685bfd614088f9e4d284cf438b3edc1c71
Roza Raimova <rraimova02@gmail.com>
2023-12-09 00:24:29 +0600

    Added failures

---
+++ lib/core/error/failures.dart
@@ -0,0 +1,18 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+class ServerFailure extends Failure {}
+
+class CacheFailure extends Failure {}
+
+class NetworkFailure extends Failure {}
+
+class ExceptionFailure extends Failure {}
+
+class CredentialFailure extends Failure {}
+
+class AuthenticationFailure extends Failure {}
